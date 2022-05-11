14afdb759417e211c8cc57f0d19ec0318712d174
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-05-11 21:01:34 +0700

    uri, id in payload

--- src/controllers/account/account.controller.ts
+++ src/controllers/account/account.controller.ts
@@ -3,12 +3,12 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { AccountService } from '../../services/account/account.service';
 
-@Controller('account')
+@Controller('api/helio/account')
 export class AccountController {
   constructor(private readonly accountService: AccountService) { }
 
   @UseGuards(LocalAuthGuard)
-  @Post('login')
+  @Post('signin')
   async login(@Request() req) {
     return this.accountService.login(req.user)
   }
