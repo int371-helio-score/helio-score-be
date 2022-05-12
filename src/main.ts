ad39306a311cfc33b3635c870a6fe04dd3e54a6e
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-05-12 13:16:45 +0700

    enable CORs

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(
+    {
+      origin: ['http://localhost:8080/'],
+      methods: ['POST', 'PUT', 'DELETE', 'GET']
+    }
+  );
   await app.listen(3000);
 }
 bootstrap();
