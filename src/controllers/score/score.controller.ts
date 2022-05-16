5daa6013998366e8f053ad0dad7a93b0fbb59f53
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-05-16 22:19:00 +0700

    getAllScores by classId for teacher

--- src/controllers/score/score.controller.ts
+++ src/controllers/score/score.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { getScoreDto } from 'src/dto/score/create-score.dto';
 import { upload } from 'src/services/common/common.service';
 import { ScoreService } from '../../services/score/score.service';
 
 
-@Controller('score')
+@Controller('api/helio/score')
 export class ScoreController {
   constructor(private readonly scoreService: ScoreService) { }
 
@@ -13,5 +14,17 @@ export class ScoreController {
     FileFieldsInterceptor([{ name: 'file', maxCount: 1 }], upload))
   importScore(@Body() request: any) {
     return this.scoreService.importScore(request)
+  }
+
+  @Get()
+  getScoresByClass(@Body() param: getScoreDto) {
+    try {
+      return this.scoreService.getAllScoresByClassId(param.class_id)
+    } catch (err: any) {
+      return {
+        statusCode: err.statuscode,
+        message: err.originalError
+      }
+    }
   }
 }
--- src/dto/score/create-score.dto.ts
+++ src/dto/score/create-score.dto.ts
@@ -1,1 +1,8 @@
-export class CreateScoreDto {}
+import { IsString } from "class-validator";
+
+export class CreateScoreDto { }
+
+export class getScoreDto {
+    @IsString()
+    class_id: string
+}
