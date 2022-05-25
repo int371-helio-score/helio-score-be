c3184a301d7c4b18f09f00683b30bba22eedbcc9
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-05-25 14:56:27 +0700

    fix getSubject param

--- src/controllers/subject/subject.controller.ts
+++ src/controllers/subject/subject.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { getSubjectsByAcademicSemesterDto } from 'src/dto/subject/create-subject.dto';
 import { SubjectService } from '../../services/subject/subject.service';
@@ -8,8 +8,8 @@ export class SubjectController {
   constructor(private readonly subjectService: SubjectService) { }
 
   @UseGuards(JwtAuthGuard)
-  @Get(':semester/:academicYear')
-  getSubjects(@Request() request: any, @Param() params: getSubjectsByAcademicSemesterDto) {
+  @Get()
+  getSubjects(@Request() request: any, @Query() params: getSubjectsByAcademicSemesterDto) {
     try {
       return this.subjectService.getSubjectsByAcademicSemester(request.user.userId, params)
     } catch (err: any) {
