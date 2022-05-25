c96555c3e2804a68b189bb0aaee071aea2d2227c
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-05-25 21:51:17 +0700

    getAccountInfo

--- src/services/account/account.service.ts
+++ src/services/account/account.service.ts
@@ -3,14 +3,15 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CommonService } from 'src/services/common/common.service';
 import { Account } from 'src/entities/account.entity';
-import { Repository } from 'typeorm';
+import { MongoRepository } from 'typeorm';
+import mongoose from 'mongoose';
 
 @Injectable()
 export class AccountService {
   constructor(
     private jwtService: JwtService,
     @InjectRepository(Account)
-    private repo: Repository<Account>
+    private repo: MongoRepository<Account>
   ) { }
 
   @Inject()
@@ -38,6 +39,28 @@ export class AccountService {
       statusCode: 200,
       message: "success",
       data: { token: `Bearer ${this.jwtService.sign(payload)}` }
+    }
+  }
+
+  async getInfo(user: any) {
+    const result = await this.repo.findBy({ where: { _id: new mongoose.Types.ObjectId(user.userId) } })
+    if (result.length == 0) {
+      return {
+        statusCode: 404,
+        message: "No Records."
+      }
+    }
+
+    const obj = {
+      firstName: result[0].firstName,
+      lastName: result[0].lastName,
+      email: result[0].email
+    }
+
+    return {
+      statusCode: 200,
+      message: "success",
+      data: obj
     }
   }
 }
