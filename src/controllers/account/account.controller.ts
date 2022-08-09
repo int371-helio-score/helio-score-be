377fa77a197c8e69be93da43fe9485e1a28ef72f
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-08-09 13:29:19 +0700

    Google SSO

--- src/controllers/account/account.controller.ts
+++ src/controllers/account/account.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Post, Get, Request, UseGuards, Req } from '@nestjs/common';
+import { GoogleAuthGuard } from 'src/auth/google-auth.guard';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
 import { AccountService } from '../../services/account/account.service';
@@ -8,7 +9,7 @@ export class AccountController {
   constructor(private readonly accountService: AccountService) { }
 
   @UseGuards(LocalAuthGuard)
-  @Post('signin')
+  @Post('login')
   async login(@Request() req) {
     try {
       return this.accountService.login(req.user)
@@ -33,6 +34,16 @@ export class AccountController {
       }
     }
 
+  }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('auth/google')
+  async googleLogin() { }
+
+  @UseGuards(GoogleAuthGuard)
+  @Get('google/redirect')
+  async googleRedirect(@Req() req) {
+    return this.accountService.loginWithGoogle(req)
   }
 
 }
