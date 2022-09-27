13b76d1d840fb6231926f289b2829e563df787d4
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-09-27 15:30:42 +0700

    deploy-test.yml, cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors(
     {
-      origin: ['http://localhost:8080', 'https://helioscore.sytes.net'],
+      origin: ['http://localhost:8080', 'https://helioscore.sytes.net', 'http://20.82.6.4:80'],
       methods: ['POST', 'PATCH', 'DELETE', 'GET']
     }
   );
