e80d6895d70e02568738affa4d5afc4aa2f8a1f3
zyict <thanyaluck.yict@mail.kmutt.ac.th>
2022-10-19 20:04:04 +0700

    edit/delete class

--- src/controllers/class/class.controller.ts
+++ src/controllers/class/class.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Param, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Request, UseGuards, Patch, Delete } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { CreateClassDto, GetAllClassBySubjectDto } from 'src/dto/class/create-class.dto';
+import { CreateClassDto, EditClassDto, GetAllClassBySubjectDto } from 'src/dto/class/create-class.dto';
 import { ClassService } from '../../services/class/class.service';
 
 @Controller('api/helio/class')
@@ -25,6 +25,32 @@ export class ClassController {
   async createClassInSubject(@Body() body: CreateClassDto) {
     try {
       return await this.classService.createClass(body.subjectId, body.class)
+    } catch (err: any) {
+      return {
+        statusCode: err.statuscode,
+        message: err.originalError
+      }
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch()
+  async editClass(@Body() body: EditClassDto) {
+    try {
+      return await this.classService.editClass(body)
+    } catch (err: any) {
+      return {
+        statusCode: err.statuscode,
+        message: err.originalError
+      }
+    }
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':classId')
+  async deleteClass(@Param('classId') classId: string) {
+    try {
+      return await this.classService.deleteClass(classId)
     } catch (err: any) {
       return {
         statusCode: err.statuscode,
